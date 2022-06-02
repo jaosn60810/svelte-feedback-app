c3c054e40320a93c87cf2ffdb2a511320f6c5fe3
jason60810 <jason60810@gmail.com>
2022-06-02 23:10:02 +0800

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import FeedbackForm from './lib/FeedbackForm.svelte';
+  import FeedbackList from './lib/FeedbackList.svelte';
+  import FeedbackStats from './lib/FeedbackStats.svelte';
+</script>
+
+<main class="container">
+  <FeedbackForm />
+  <FeedbackStats />
+  <FeedbackList />
+</main>
+
+<style>
+</style>
